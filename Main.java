83039f4d5e38c60d6c42df2e3c3e47bee3d5e4ac
ElijahWargo <Elijah.wargo@patelhs.org>
2020-11-13 23:27:24 +0000

    Added a way for user to enter each students name

--- Main.java
+++ Main.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
-}
+public static void main(String[] args) {
+  Scanner scan = new Scanner(System.in);
+  //scanner
+   System.out.println("What is the name of your 1st student?");
+  String name1 = scan.next();
+  //1st kids name
+     System.out.println("What is the name of your 2nd student?");
+  String name2 = scan.next();
+  //2nd kids name
+     System.out.println("What is the name of your 3rd student?");
+  String name3 = scan.next();
+  //3rd kids name
+     System.out.println("What is the name of your 4th student?");
+  String name4 = scan.next();
+  //4th kids name
+     System.out.println("What is the name of your 5th student?");
+  String name5 = scan.next();
+  //5th kids name
+     System.out.println("What is the name of your 6th student?");
+  String name6 = scan.next();
+  //6th kids name
+     System.out.println("What is the name of your 7th student?");
+  String name7 = scan.next();
+  //7th kids name
+     System.out.println("What is the name of your 8th student?");
+  String name8 = scan.next();
+  //8th kids name
+     System.out.println("What is the name of your 9th student?");
+  String name9 = scan.next();
+  //9th kids name
+     System.out.println("What is the name of your 10th student?");
+  String name10 = scan.next();
+  //10th kids name
+     System.out.println("What is the name of your 11th student?");
+  String name11 = scan.next();
+  //11th kids name
+     System.out.println("What is the name of your 12th student?");
+  String name12 = scan.next();
+  //12th kids name
+     System.out.println("What is the name of your 13th student?");
+  String name13 = scan.next();
+  //13th kids name
+     System.out.println("What is the name of your 14th student?");
+  String name14 = scan.next();
+  //14th kids name
+     System.out.println("What is the name of your 15th student?");
+  String name15 = scan.next();
+  //15th kids name
+ 
