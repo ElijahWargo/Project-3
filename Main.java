5378f6f38134f00378163bacd4c406b56dd66962
ElijahWargo <Elijah.wargo@patelhs.org>
2020-11-14 00:31:46 +0000

    Fixed very minor errors and made comments easier to understand

--- Main.java
+++ Main.java
@@ -2,64 +2,64 @@ import java.util.Scanner;
 class Main {
 public static void main(String[] args) {
 Scanner scan = new Scanner(System.in);
-//scanner
+//Scanner
  System.out.println("What is the name of your 1st student?");
 String name1 = scan.next();
-//first students name
+//1st Students Name 
    System.out.println("What is the name of your 2nd student?");
 String name2 = scan.next();
-//second kids name
+//2nd Students Name 
    System.out.println("What is the name of your 3rd student?");
 String name3 = scan.next();
-//3rd kids name
+//3rd Students Name 
    System.out.println("What is the name of your 4th student?");
 String name4 = scan.next();
-//4th kids name
+//4th Students Name 
    System.out.println("What is the name of your 5th student?");
 String name5 = scan.next();
-//5th kids name
+//5th Students Name 
    System.out.println("What is the name of your 6th student?");
 String name6 = scan.next();
-//6th kids name
+//6th Students Name 
    System.out.println("What is the name of your 7th student?");
 String name7 = scan.next();
-//7th kids name
+//7th Students Name 
    System.out.println("What is the name of your 8th student?");
 String name8 = scan.next();
-//8th kids name
+//8th Students Name 
    System.out.println("What is the name of your 9th student?");
 String name9 = scan.next();
-//9th kids name
+//9th Students Name 
    System.out.println("What is the name of your 10th student?");
 String name10 = scan.next();
-//10th kids name
+//10th Students Name 
    System.out.println("What is the name of your 11th student?");
 String name11 = scan.next();
-//11th kids name
+//11th Students Name 
    System.out.println("What is the name of your 12th student?");
 String name12 = scan.next();
-//12th kids name
+//12th Students Name 
    System.out.println("What is the name of your 13th student?");
 String name13 = scan.next();
-//13th kids name
+//13th Students Name 
    System.out.println("What is the name of your 14th student?");
 String name14 = scan.next();
-//14th kids name
+//14th Students Name 
    System.out.println("What is the name of your 15th student?");
 String name15 = scan.next();
-//15th kids name
+//15th Students Name 
    String[ ] studentNames = {name1, name2, name3, name4, name5, name6, name7, name8, name9, name10, name11, name12, name13, name14, name15};
-// array to hold the input from the user
+// Made and array to hold the input that the user inputed
  
      int[ ] seatNumbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
      //Array for all of the seat numbers
        for(int a = 0; a < 1; a++){
-       // for loop that tells the cod how many times to run
+       // Created a for loop that tells the code how many times to run
          int count = 0;
-         //initial value for count
+         //The initial value for count
          while(count <= 14) {System.out.println(studentNames[count] + " Will sit in seat number" + seatNumbers[count]);
          count++;
-         // while loop that prints the students names and seat numbers
+         //Made a while loop that prints the students names and seat numbers without it being infinite
      }
    }
  }
